956e0c153123ad55a028e2bebf3fb2c85ec14144
Ivan Lynch <ivan.lynch@outlook.com.ar>
2016-09-10 15:35:26 -0300

    Instalacion de Jade, creacion de estructura y render del index.jade

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-//Require de express
+//Hacemos el require de express
 var express = require('express');
 //Declaramos la variable donde se ejectuta express
 var app = express();
+//Le decimos a la aplicacion que las visas se van a hacer con jade
+app.set("view engine", "jade");
 
-/* Se ejecuta al momento de ingresar al servidor "localhost:8080" y devuelve una respuesta */
-
+/* Se ejecuta al momento de ingresar al servidor "localhost:8080" y devuelve una respuesta 
+en este caso hace el render del index */ 
 app.get("/", function(solicitud, respuesta){
-	respuesta.send("Hola Mundo");
+	respuesta.render("index");
 });
 
 //Le decimos a express que escuche en el puerto 8080
