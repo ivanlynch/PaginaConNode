59a3b68ef323a059d170745ad92da920fc791e4a
Ivan Lynch <ivan.lynch@outlook.com.ar>
2016-09-10 22:04:54 -0300

    Conexion con MongoDB, declaracion del Schema y el Model

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-//Hacemos el require de express
+//Require de los package que se utilizan para la aplicacion
 var express = require('express');
-//Declaramos la variable donde se ejectuta express
+var mongoose = require('mongoose');
+
+//Variable con la ejecucion de Express
 var app = express();
+
+//Conexion con la base de datos
+mongoose.connect('mongodb://localhost/paginawebconnode');
+
+//Definicion del Schema que se va a usar para almacenar los datos en MongoDB
+var productSchema = {
+	title: String,
+	description: String,
+	imageUrl: String,
+	pricing: Number
+};
+
+//Declaracion del model Product
+var Product = mongoose.model("Product", productSchema);
+
 //Le decimos a la aplicacion que las visas se van a hacer con jade
 app.set("view engine", "jade");
 
